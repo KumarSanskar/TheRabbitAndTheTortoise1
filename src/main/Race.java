ffba117f15d4ce9f8f3f443552c3da2c397f44be
Suyash <suyash.sri,0902@gmail.com>
2020-10-30 23:42:35 +0530

    Initial commit 1

---
+++ src/main/Race.java
@@ -0,0 +1,12 @@
+package main;
+
+import thread.rabbit.Rabbit;
+
+public class Race {
+    public static void main(String[] args) {
+        Rabbit rabbit = new Rabbit();
+        rabbit.getRabbit().start();
+        Tortoise tortoise = new Tortoise();
+        tortoise.start();
+    }
+}
